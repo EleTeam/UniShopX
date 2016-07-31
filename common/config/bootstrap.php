b1fda1bf1ec411c96203c0e807aa6e3c2cdd5894
tony <908601756@qq.com>
2016-07-31 14:58:36 +0800

    使用外网地址

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -15,4 +15,5 @@ Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
 Yii::setAlias('@data', dirname(dirname(__DIR__)) . '/data');
-Yii::setAlias('@imghost', 'http://img.eleteam.com');
+//Yii::setAlias('@imghost', 'http://local.eleteamdata.ygcr8.com'); //本地地址
+Yii::setAlias('@imghost', 'http://eleteamdata.ygcr8.com'); //外网地址
