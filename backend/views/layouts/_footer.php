ee0879e3cc331a96e161b929b724cd45a3ec1a20
tony <908601756@qq.com>
2016-08-01 22:24:13 +0800

    后台用户登录

---
+++ backend/views/layouts/_footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0.0
+    </div>
+    <strong>Copyright &copy; 2016 <a href="http://eleteam.ygcr8.com">Eleteam</a>.</strong> All rights
+    reserved.
+</footer>
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -257,7 +257,7 @@ AppAsset::register($this);
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <img src="adminlte/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                            <span class="hidden-xs">Alexander Pierce</span>
+                            <span class="hidden-xs"><?=Yii::$app->user->identity->username?></span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- User image -->
@@ -290,7 +290,7 @@ AppAsset::register($this);
                                     <a href="#" class="btn btn-default btn-flat">Profile</a>
                                 </div>
                                 <div class="pull-right">
-                                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                    <a href="<?=Url::toRoute('/user/logout')?>" class="btn btn-default btn-flat"><?=Yii::t('app', 'Logout')?></a>
                                 </div>
                             </li>
                         </ul>
@@ -492,13 +492,7 @@ AppAsset::register($this);
     </div>
     <!-- /.content-wrapper -->
 
-    <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-            <b>Version</b> 2.3.3
-        </div>
-        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-        reserved.
-    </footer>
+    <?php include_once '_footer.php'?>
 
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark">
